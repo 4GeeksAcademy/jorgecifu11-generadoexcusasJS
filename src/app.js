57ea7f679a4a06b6c691558047f4b9b41d69d671
Jorge Cifuentes Pérez <jorge.cifuentes.perez@gmail.com>
2025-05-05 18:55:01 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function ramdonExcuse() {
+  
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  const whoIndexRamdon = Math.floor(Math.random() * who.length)
+  const actionIndexRamdon = Math.floor(Math.random() * action.length)
+  const whatIndexRamdon = Math.floor(Math.random() * what.length)
+  const whenIndexRamdon = Math.floor(Math.random() * when.length)
+
+  const excuse = '${who[whoIndexRamdon]} ${action[actionIndexRamdon]} ${what[whatIndexRamdon]} ${when[whenIndexRamdon]}'
+
+  return excuse
+}
+
+window.onload = function () {
+  const p = document.getElementById("excuse")
+  const excuse = ramdonExcuse()
+  p.innerHTML = ramdonExcuse()
 };
